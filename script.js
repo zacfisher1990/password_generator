96ac1abe3f99999af7fa288a6e2f385c3e69e3f5
unknown <zcfshr@gmail.com>
2022-07-30 13:58:34 -0600

    added if to password length and alert window

--- script.js
+++ script.js
@@ -2,26 +2,33 @@
 
 var generateBtn = document.querySelector("#generate");
 
-//code for each criteria password prompt
 
 //sets character length by user through prompt and stores value in userInputLength
 function userInputLength() {
   let passwordLength = prompt("Enter length of password \nMust be between 8 and 128 characters.");
+  if (passwordLength < 8 || passwordLength > 128) { //if password is shorter than 8 characters or longer than 128
+    alert("Password length must be between 8 and 128 characters."); //alert message
+    window.location = 'C:/Users/Zac/OneDrive/Desktop/challenge_3/02-Challenge/Develop/index.html'.reload(); //page reloads
+  } 
   return passwordLength;
 }
-//sets character types in password
+
+//sets whether or not user wants special characters
 function userInputSpecialChar() {
-  let passwordType = confirm("Would you like password to include special characters? \n(!@#$%^&*()`~-=_+[]{}?><:;',./)");
-  if (passwordType) {
+  let specialChar = confirm("Would you like password to include special characters? \n(!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)\nClick 'OK' for yes or 'Cancel' for no.");
+  if (specialChar) {
     charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()`~-=_+[]{}?><:;',./"
   } else {
     charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
   }
-  return passwordType;
+  return specialChar;
 }
+//sets whether or not user wants uppercase letters
 
+//sets whether or not user wants lower case letters
 
-//
+//sets whether or not user wants 
+
 function generatePassword() {
   var length = userInputLength(); //character length is defined by userInputLength
       type = userInputSpecialChar(); //character type is defined by userInputSpecialChar
